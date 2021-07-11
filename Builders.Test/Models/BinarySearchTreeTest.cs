7fc9ae7f42d8cc0f41bc6cd54d7e5051bba63e09
Rafael Ahrons <rafael.ahrons@gmail.com>
2021-07-10 21:38:47 -0300

    Add tests for binary search tree

---
+++ Builders.Test/Models/BinarySearchTreeTest.cs
@@ -0,0 +1,69 @@
+using Builders.Models;
+using Xunit;
+
+namespace Builders.Test.Models
+{
+    public class BinarySearchTreeTest
+    {
+        [Fact]
+        public void ShouldBeAbleToAddInTheRightPositionOfTreeRootGivingValueBiggerThanTheRootValue()
+        {
+            #region Arrange
+            var bst = new BinarySearchTree();
+            bst.AddNode(1);
+            bst.AddNode(2);
+
+            var expectedValue = 2;
+            #endregion Arrange
+
+            #region Act
+            var rightNode = bst.root.Right;
+            #endregion Act
+
+            #region Assert
+            Assert.NotNull(rightNode);
+            Assert.Equal(rightNode.Value, expectedValue);
+            #endregion Assert
+        }
+
+        [Fact]
+        public void ShouldBeAbleToAddInTheLeftPositionOfTreeRootGivingValueLesThanTheRootValue()
+        {
+            #region Arrange
+            var bst = new BinarySearchTree();
+            bst.AddNode(3);
+            bst.AddNode(2);
+
+            var expectedValue = 2;
+            #endregion Arrange
+
+            #region Act
+            var leftNode = bst.root.Left;
+            #endregion Act
+
+            #region Assert
+            Assert.NotNull(leftNode);
+            Assert.Equal(leftNode.Value, expectedValue);
+            #endregion Assert
+        }
+
+        public void ShouldBeAbleToAddInTheRootPositionGivingRootIsNull()
+        {
+            #region Arrange
+            var bst = new BinarySearchTree();
+            bst.AddNode(3);
+
+            var expectedValue = 3;
+            #endregion Arrange
+
+            #region Act
+            var rootNode = bst.root;
+            #endregion Act
+
+            #region Assert
+            Assert.NotNull(rootNode);
+            Assert.Equal(rootNode.Value, expectedValue);
+            #endregion Assert
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,21 @@
+# Builders
+
+It's a .Net 5 project that implements a Binary Search Tree.
+
+## Dependencies
+
+- .Net 5 installed
+
+## How to run
+
+To run the project execute the following command:
+
+```bash
+dotnet run .\Builders\
+```
+
+To execute tests, you can run inside the Builders.Test project folder:
+
+```bash
+dotnet test .\Builders.Test\
+```
