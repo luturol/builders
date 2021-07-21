18925693fb2f6049779088abb4c859953e356ed1
Rafael Ahrons <rafael.ahrons@gmail.com>
2021-07-21 04:29:59 -0300

    Add dockerfile
    removed https redirection

--- Builders/Startup.cs
+++ Builders/Startup.cs
@@ -24,6 +24,11 @@ namespace Builders
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+             services.AddHttpsRedirection(options =>
+            {
+                options.HttpsPort = 443;
+            });
+
             services.Configure<BinarySearchTreeDbSettings>(Configuration.GetSection(nameof(BinarySearchTreeDbSettings)));
             services.AddSingleton<IMongoDbSettings>(sp => sp.GetService<IOptions<BinarySearchTreeDbSettings>>().Value);
 
@@ -47,7 +52,7 @@ namespace Builders
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Builders v1"));
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseRouting();
 
